59bb72ed8bde22c392572a6e0d3f429f5a6b3a8c
Vinicius Koga <viniciuskoga_m@hotmail.com>
2023-01-26 14:40:39 -0300

    finished show header on scroll-up fn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
-//HEADER SHOW ON SCROLL-UP FUNCTION
+//SHOW HEADER ON SCROLL-UP FN
 let lastScrollTop = 0;
 let header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     let scrollTop = window.pageYOffset
     if (scrollTop > lastScrollTop) {
         header.style.top = '-71px';
+        header.style.opacity = '0'
     } else {
         header.style.top = '0px';
+        header.style.opacity = '1'
     }
     lastScrollTop = scrollTop;
 });
