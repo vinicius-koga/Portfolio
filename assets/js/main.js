007aba74ae198c92e1a3cc17133f7ece93d4ee5c
Vinicius Koga <viniciuskoga_m@hotmail.com>
2023-01-30 18:24:32 -0300

    finished section projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,22 @@ function typeTextAnimation(el) {
 
 const insertText = document.querySelector('.typeTextAnimation');
 typeTextAnimation(insertText);
+
+
+//SHOW MORE PROJECTS ON MOBILE FN
+function showMore() {
+    let mobileHidden = document.querySelectorAll('.project-area .mobileHidden');
+    let mobileHiddenBtn = document.querySelector('.project-area .seeMoreBtn');
+    mobileHidden.forEach(i => {
+        if (i.classList.contains('d-none')) {
+            i.classList.remove('d-none');
+            i.classList.add('d-flex');
+            mobileHiddenBtn.innerHTML = 'Ver menos'
+        } else if (i.classList.contains('d-flex')) {
+            i.classList.remove('d-flex');
+            i.classList.add('d-none');
+            mobileHiddenBtn.innerHTML = 'Ver mais'
+        }
+    })
+
+}
